12131b4fd61196764df347ef23bf99dacc3431af
Kamandar <kamandaragasafli@users.noreply.github.com>
2026-09-10 15:43:33 +0400

    first commit4

--- config/settings.py
+++ config/settings.py
@@ -139,7 +139,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+# Statik faylların URL ünvanı
+STATIC_URL = '/static/'
+
+# collectstatic əmrinin faylları toplayacağı qovluq (Bu sətri əlavə edin)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
